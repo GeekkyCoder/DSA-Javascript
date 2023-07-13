78eddabdb89f3b6e53a714c5f1706c5dfc1db3b7
Faraz <farazatgithub@gmail.com>
2023-07-13 08:20:06 +0500

    doubly linked list

--- linkedlist/doubly_linked_list.js
+++ linkedlist/doubly_linked_list.js
@@ -1,137 +1,124 @@
-// visuallizing Doubly linked list in javascript using hashmap ->> objects
-
-// const DoublylinkedList = {
-//   value: 10,
-//   next: {
-//     value: 5,
-//     prev:null,
-//     next: {
-//       value: 16,
-//       next: null,
-//       prev:5
-//     },
-//   },
-// };
-
-// creacting a linked list in js
-class DoublylinkedList {
+// DoublyLinkedList
+class DoublyNode {
   constructor(value) {
-    this.head = {
-      value: value,
-      next: null,
-      prev: null,
-    };
-    this.tail = this.head;
-    this.length = 1;
-  }
-
-  append(value) {
-    // appends a new node, so now the tail will be newNode
-    const newNode = {
-      value,
-      next: null,
-      prev: null,
-    };
-    newNode.prev = this.tail;
-    this.tail.next = newNode;
-    this.tail = newNode;
-    this.length++;
-    return this;
-  }
-
-  // prepend -> add the value before the head, so it becomes new head node
-  prepend(value) {
-    // updates the head node, which will be newValue
-    // eg: -> 1 --> 10 --> 5 -->16
-    const newNode = {
-      value: value,
-      next: null,
-      prev: null,
-    };
-
-    newNode.next = this.head;
-    this.head.prev = newNode;
-    this.head = newNode;
-    this.length++;
-    return this;
-  }
-
-  insert(index, value) {
-    // if the index at which value to be inserted is greater than the length, add the value to th end
-    if (index >= this.length) {
-      return this.append(value);
-    }
-
-    // add the value to particular index
-
-    const newNode = {
-      value: value,
-      next: null,
-      prev: null,
-    };
-    // traversing to index - 1 to get the 10, because we want to add the new node after 10 and before 5
-    const leader = this.traverseToIndex(index - 1);
-
-    /* visual
-    10       5
-    *       *
-      \ 99 |
-        *
-    */
-
-    // 5
-    newNode.prev = leader;
-    const follower = leader.next;
-    leader.next = newNode;
-    newNode.prev = leader;
-    newNode.next = follower;
-    follower.prev = newNode;
-
-    this.length++;
-
-    // 10 --> 99 --> 5
-    return this;
-  }
-
-  traverseToIndex(index) {
-    let currentNode = this.head;
-    let counter = 0;
-    while (counter !== index) {
-      currentNode = currentNode.next;
-      counter++;
-    }
-
-    // {value:10,next:{value:5,...}}, now we have the 10
-    return currentNode;
-  }
-
-  remove(index) {
-    if (index > this.length) {
-      return "invalid index";
-    }
-    const leader = this.traverseToIndex(index - 1);
-    const temp = leader.next.next;
-    delete leader.next;
-    leader.next = temp;
-    this.length--;
-  }
-
-  printList() {
-    const values = [];
-    let currentNode = this.head;
-    while (currentNode !== null) {
-      values.push(currentNode.value);
-      currentNode = currentNode.next;
-    }
-    return values;
+    this.value = value,
+      this.next = null,
+      this.prev = null
   }
 }
 
-const myDoublyLinkedList = new DoublylinkedList(10);
+class DoublyLinkedList {
+  constructor() {
+    this.head = null
+    this.tail = this.head,
+    this.prev = null
+    this.length = 0
+  }
 
-myDoublyLinkedList.append(2);
-myDoublyLinkedList.append(55);
-myDoublyLinkedList.prepend(100);
-console.log(myDoublyLinkedList.insert(2, 76));
+  // add at beginning 0(1)
+  prepend(value) {
+    const newNode = new DoublyNode(value)
+    if (!this.head) {
+      this.head = newNode
+      this.tail = this.head
+      this.length++
+    } else {
+      // old head 
+      const oldHead = this.head
+      this.head.prev = newNode
+      this.head = newNode
+      this.head.next = oldHead
+      this.length++
+    }
+  }
 
-console.log(myDoublyLinkedList.printList());
+  // add at end 0(1)
+  append(value) {
+    const newNode = new DoublyNode(value)
+    if (this.tail) {
+      newNode.prev = this.tail
+      this.tail.next = newNode
+      this.tail = newNode
+      this.length++
+    }
+
+    return undefined
+  }
+
+  // inserting at index 0(n)
+  insert(index, value) {
+    const newNode = new DoublyNode(value)
+    const leader = this.traverseToIndex(index - 1)
+    const tempHolder = leader.next
+    newNode.prev = leader
+    leader.next = newNode
+    newNode.next = tempHolder
+    tempHolder.prev = newNode
+    this.length++
+  }
+
+  // removing at partilcular index 0(n)
+  remove(index) {
+    const leader = this.traverseToIndex(index - 1)
+    leader.next.next.prev = leader
+    leader.next = leader.next.next
+    this.length--
+  }
+
+  traverseToIndex(index) {
+    let currentNode = this.head
+    for (let i = 0; i < index; i++) {
+      currentNode = currentNode.next
+    }
+
+    return currentNode
+  }
+
+
+  // searching 0(n)
+  search(value) {
+    let currentNode = this.head
+    while (currentNode) {
+      if (currentNode.value === value) {
+        return currentNode
+      }
+      currentNode = currentNode.next
+    }
+
+    return "not found"
+  }
+
+  //0(n)
+  traverse() {
+    let currentNode = this.head
+    let values = []
+    while (currentNode) {
+      values.push(currentNode.value)
+      currentNode = currentNode.next
+    }
+
+    return values
+  }
+
+}
+
+const myDoublyLinkedList = new DoublyLinkedList()
+myDoublyLinkedList.prepend(10)
+myDoublyLinkedList.prepend(20)
+myDoublyLinkedList.append(30)
+myDoublyLinkedList.append(50)
+myDoublyLinkedList.insert(2,70)
+myDoublyLinkedList.remove(2)
+console.log(myDoublyLinkedList.search(10))
+console.log(myDoublyLinkedList.traverse())
+console.log(myDoublyLinkedList)
+
+
+
+// time complexities of Doubly linked list: 
+// searching:0(n)
+// traverse: 0(n)
+// insert: 0(n)
+// remove: 0(n)
+// prepend: 0(1)
+// append: 0(1)
